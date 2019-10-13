e5a8a184b3ec03c871aae45cd5c89268256a6e52
Yaazhisai <Yaazhisai@192.168.0.107>
2019-10-13 23:21:57 +0530

    Ex-Wait Added

--- src/main/java/Acme/Acme/AceVendorSearch.java
+++ src/main/java/Acme/Acme/AceVendorSearch.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 
@@ -14,6 +16,8 @@ public class AceVendorSearch {
 	public static void aceVendor() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
+		@SuppressWarnings("deprecation")
+		WebDriverWait wait = new WebDriverWait(driver, 10);
 		driver.get("https://acme-test.uipath.com/account/login");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -23,11 +27,12 @@ public class AceVendorSearch {
 		email.sendKeys(Keys.TAB);
 		driver.findElementById("password").sendKeys("leaf@12");
 		driver.findElementById("buttonLogin").click();
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 		WebElement Vendorbutton = driver.findElementByXPath("//button[text()[contains(.,'Vendors')]]");
+		wait.until(ExpectedConditions.elementToBeClickable(Vendorbutton));
 		action.moveToElement(Vendorbutton).click().perform();
-		Thread.sleep(2000);
 		WebElement Search = driver.findElementByXPath("//a[contains(text(),'Search for Vendor')]");
+		wait.until(ExpectedConditions.elementToBeClickable(Search));
 		action.moveToElement(Search).click().perform();
 		driver.findElementById("vendorName").sendKeys("Blue Lagoon");
 		driver.findElementById("buttonSearch").click();
